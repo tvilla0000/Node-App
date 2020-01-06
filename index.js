360f94396c6f21363c487d077d7428d0d9e0d02e
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2020-01-06 15:59:30 -0800

    Initialize server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-s;
+/*
+ *   Primary file for API
+ *
+ */
+
+// Dependencies
+
+const http = require("http");
+
+// The server should respond to all requests with a string
+
+const server = http.createServer(function(req, res) {
+  res.end("Hello World\n");
+});
+
+// Start the server, and listen on port 3000
+server.listen(3000, function() {
+  console.log("The server is listening on port 3000 now");
+});
