aaf33e787ba7a05d8c05c1966be24413fc5514a0
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2020-01-06 16:15:10 -0800

    After server is initialized, Parse URL and send response followed by a log of requested path.

--- index.js
+++ index.js
@@ -6,11 +6,23 @@
 // Dependencies
 
 const http = require("http");
+const url = require("url");
 
 // The server should respond to all requests with a string
 
 const server = http.createServer(function(req, res) {
+  // Get URL and Parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  // Send the response
   res.end("Hello World\n");
+
+  //Log the request path
+  console.log("Request is received on this path: " + trimmedPath);
 });
 
 // Start the server, and listen on port 3000
